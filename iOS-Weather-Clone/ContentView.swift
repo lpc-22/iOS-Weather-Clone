c3b883a73737c00f321dd8ea665bbe0d642f6027
pc22 <isaaclpc0622@gmail.com>
2024-03-18 22:23:41 -0400

    Initial Commit

---
+++ iOS-Weather-Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-Weather-Clone
+//
+//  Created by Isaac Lee on 18/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Weather-Clone/iOS_Weather_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Weather_CloneApp.swift
+//  iOS-Weather-Clone
+//
+//  Created by Isaac Lee on 18/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Weather_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
